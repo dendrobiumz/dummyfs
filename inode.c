1565373f68247fed101d4e00eb038ef341e189e7
dendrobiumz <page19920610@gmail.com>
2024-08-25 21:25:13 +0800

    [update] add some function comment.

--- inode.c
+++ inode.c
@@ -1,9 +1,25 @@
 #include <linux/fs.h>
 
 
+static int dummyfs_get_inode(struct super_block *sb, unsigned long i_no)
+{
+    return 0;
+}
+
+
+
+/*
+ * called when the VFS needs to look up an inode in a parent directory.
+ * The name to look for is found in the dentry. 
+ * This method must call d_add() to insert the found inode into the dentry.  
+ * The “i_count” field in the inode structure should be incremented. 
+ * 
+ */
 struct dentry *dummyfs_lookup(struct inode *i, struct dentry *d, unsigned int flag)
 {
+    struct dentry *ret;
 
+    return ret;
 }
 
 
@@ -38,7 +54,10 @@ int dummyfs_rmdir(struct inode *i, struct dentry *d)
     return 0;
 }
 
-
+/*
+ * called by the rename(2) system call to rename the object to have the parent 
+ * and name given by the second inode and dentry.
+ */
 int dummyfs_rename(struct mnt_idmap *id,
                     struct inode *i1,
                     struct dentry *d1,
@@ -49,7 +68,11 @@ int dummyfs_rename(struct mnt_idmap *id,
     return 0;
 }
 
-
+/*
+ * called by the link(2) system call
+ * Only required if you want to support hard links.
+ * You will probably need to call d_instantiate() just as you would in the create() method
+ */
 int dummyfs_link(struct dentry *src, struct inode *i, struct dentry *dst)
 {
 
